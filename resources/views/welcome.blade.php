f5eed1896f3a5a2afa3352803497b962c3dd667d
Hancy Khalid <67202148+hancy8@users.noreply.github.com>
2023-07-24 17:23:36 +0500

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log_in</title>
+</head>
+<body>
+    
+    <h1>Log in sign up page</h1>
+    @if($errors->has('email'))
+    <div class="alert alert-danger">{{ $errors->first('email') }}</div>
+@endif
+    <form action="{{url('/')}}/login" method="post">
+        @csrf
+    <label for="email">Enter your Email</label>
+    <input type="text" name="email" id="email" placeholder="Enter your Email here">
+    <label for="email">Enter your Password</label>
+    <input type="password" name="password" id="password" placeholder="Enter your Password here">
+    <button>Submit</button>
+    <a href="/register"> <p>register here</p></a>
+    
+</form>
+</body>
+</html>
